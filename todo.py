52010b4f5e6db9a3b23b3c0fc15c50e7b2475b7e
Wesley Ihezuo <wihezuo@gmail.com>
2023-03-02 08:49:40 -0500

    hi

--- todo.py
+++ todo.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 
 
 app = Flask(__name__)
+
+bucketlist= ["skydiving","Ear-piercings"]
 
 @app.route("/todo")
 def index():
     return render_template(
         "todo.html.jinja",
-        my_variable="Full cowling!!!",
-        list= ["skydiving","Ear-piercings", ]
+        bucketlist=bucketlist
     )
 @app.route("/add", methods=['POST'])
 def add():
     new_todo= request.form['new_todo']
-    return new_todo
 
+    bucketlist.append(new_todo)
+    return redirect(('/todo'))
 
     
